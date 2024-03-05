72d426195639acba2d90e293d3ca07e47dbebbdd
Chris <christopherperrault@hotmail.com>
2024-03-05 15:06:08 -0500

    TS update

--- app/_components/about/TechCard.tsx
+++ app/_components/about/TechCard.tsx
@@ -1,4 +1,11 @@
-export default function TechCard({ title, technologies }) {
+import React from "react";
+
+interface TechCardProps {
+  title: string;
+  technologies: string[];
+}
+
+const TechCard: React.FC<TechCardProps> = ({ title, technologies }) => {
   return (
     <div className="inline-flex m-4">
       <div className="p-4 text-5xl border-4 border-black rounded-lg bg-neutral-500">
@@ -11,4 +18,6 @@ export default function TechCard({ title, technologies }) {
       </div>
     </div>
   );
-}
+};
+
+export default TechCard;
