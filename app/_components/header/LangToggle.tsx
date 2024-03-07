e7c4286aea7e2e2ce55a4386221784e164c15974
Chris <christopherperrault@hotmail.com>
2024-03-06 23:09:52 -0500

    npm updates, react & TS upgrades, minor edit LangToggle

--- app/_components/header/LangToggle.tsx
+++ app/_components/header/LangToggle.tsx
@@ -1,5 +1,5 @@
+import { useLanguage } from "@/app/languageContext";
 import { offside } from "../../_assets/fonts/fonts";
-import { useLanguage } from "../../languageContext";
 
 export default function LangToggle() {
   const { lang, toggleLang } = useLanguage();
@@ -7,7 +7,7 @@ export default function LangToggle() {
   return (
     <div className="flex justify-end m-2 group">
       <button
-        // onClick={toggleLang}
+        onClick={toggleLang}
         className={`group rounded-full border-4 border-black p-4 bg-gradient-to-br from-yellow-300 to-yellow-600 ${offside.className} text-2xl group-hover:bg-gradient-to-br group-hover:from-yellow-500 group-hover:to-yellow-400`}
       >
         {lang ? "FRANÇAIS" : "ENGLISH"}
